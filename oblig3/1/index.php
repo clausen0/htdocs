80f5d67e02d96fca801a52bbfa4b9b9763bd2576
clausen0 <sancla@live.no>
2021-09-14 14:20:12 +0200

    feridg oppgave1 og satt opp for obligens nettside

---
+++ oblig3/1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../../ForsideStyle.css" href="style.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="../../index.php">Forside</a></li>
+        <li><a href="../index.php">tilbake</a></li>
+    </ul>
+    <ol>
+        <li>Oppgave 1 (Variabler : $a = 3 og $b = 5)</li><br>
+        <ol>
+            <li>true ($a < $b)</li><br>
+            <li>false ($a >= $b)</li><br>
+            <li>false ($a == $b)</li><br>
+            <li>false (3 ! = $b)</li><br>
+            <li>true (false == false)</li><br>
+            <li>true (($a + $b) > 7)</li><br>
+            <li>false (($b % 3) > 3)</li><br>
+            <li>true (($b % 3) >= 0)</li><br>
+        </ol>
+    </ol>
+</body>
+</html>
---
+++ oblig3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../ForsideStyle.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="../index.php">Forside</a></li>
+        <li><a href="../index.php">tilbake</a></li>
+    </ul>
+        <p><a href="1/index.php">Oppgave 1</a></p>
+</body>
+</html>
