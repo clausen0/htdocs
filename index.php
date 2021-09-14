a6077f18e92da9d8fbb1f67d328283e7d58e9a1b
clausen0 <sancla@live.no>
2021-09-14 13:16:38 +0200

    start av skoledag 14.09.2021

--- index.php
+++ index.php
@@ -14,5 +14,6 @@
         </ul>
         <p><a href="oblig1/index.php">Oblig1</a></p>
         <p><a href="oblig2/index.php">Oblig2</a></p>
+        <p><a href="oblig3/index.php">oblig3</a></p>
     </body>
 </html>
--- oblig2/1/bmikalk.php
+++ oblig2/1/bmikalk.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>BMI kalkulator</h1>
-        <form action="index.php" method="post">
+        <form action="bmikalk.php" method="post">
             <p>Hva heter du?</p>
             <input type="text" name="name"><br>
             <p>Hvor høy er du i cm?</p>
@@ -15,7 +15,7 @@
             <p>Hvor mye veier du i kg?</p>
             <input type="text" name="weight"><br>
             <p>Er du mann eller dame?</p>
-            <input type="radio" name="gender" value="male">Mann<br>
+            <input type="radio" name="gender" value="male" checked>Mann<br>
             <input type="radio" name="gender" value="female">Dame<br><br>
             <input type="submit" value="Send inn">
             <input type="reset" value="Reset">
@@ -30,14 +30,27 @@
             $hoydeIMS = $hoyde/100;
             $bmi = $vekt/($hoydeIMS*$hoydeIMS);
 
-            if ($bmi < 17){
-                echo "$navn, du er tynn"; 
+            if ($kjonn == "male") {
+                if ($bmi < 17){
+                    echo "$navn, du er tynn"; 
+                }
+                elseif($bmi >= 17 && $bmi<25){
+                    echo "$navn, du er grei";
+                }
+                else{
+                    echo "$navn, du er overvektig";
+                }
             }
-            elseif($bmi >= 17 && $bmi<25){
-                echo "$navn, du er fin";
-            }
-            else{
-                echo "$navn, du er overvektig";
+            elseif ($kjonn == "female"){
+                if ($bmi < 17){
+                    echo "$navn, du er litt under normal"; 
+                }
+                elseif($bmi >= 17 && $bmi<25){
+                    echo "$navn, du er normal";
+                }
+                else{
+                    echo "$navn, du er litt over normal";
+                }
             }
         ?>
     </body>
