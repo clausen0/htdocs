204b5f6daa814cf13e0e7de65ea073f819dccf15
clausen0 <sancla@live.no>
2022-02-15 21:25:58 +0100

    start busy prosjekt

---
+++ nettside/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Clausens Nettbutikk</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+</body>
+
+</html>
