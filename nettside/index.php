31ae41dfea17bdca9108a8b996b729d7bb5cd7fb
clausen0 <sancla@live.no>
2022-02-16 13:33:36 +0100

    nettside jobbing

--- nettside/index.php
+++ nettside/index.php
@@ -8,7 +8,27 @@
 
 <body>
     <h1>Clausens Nettbutikk</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+    <div class="template">
+        <div class="vare1">
+            <h1>Vare 1</h1>
+            <img src="./img/download.jpg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
+        
+        <div class="vare2">
+            <h1>Vare 2</h1>
+            <a href="./pages/index.php"><img src="./img/download.jpg"></a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
+
+        <div class="vare3">
+            <h1>Vare 3</h1>
+            <img src="./img/download.jpg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
+    </div>
+
 </body>
+    
 
 </html>
