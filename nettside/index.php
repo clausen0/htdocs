3280ad68f2a9166c6153830e677ed3f266218705
clausen0 <sancla@live.no>
2022-02-17 15:04:44 +0100

    nettside jobb + start på planlegging av database

--- nettside/index.php
+++ nettside/index.php
@@ -3,31 +3,48 @@
 
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?<?=filemtime("style.css")?>">
 </head>
 
 <body>
-    <h1>Clausens Nettbutikk</h1>
+    <h1 class="HovedTekst">Clausens Nettbutikk</h1>
     <div class="template">
-        <div class="vare1">
+        <div class="vare">
             <h1>Vare 1</h1>
-            <img src="./img/download.jpg">
+            <a><img src="./img/download.jpg"></a>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
         </div>
         
-        <div class="vare2">
+        <div class="vare">
             <h1>Vare 2</h1>
             <a href="./pages/index.php"><img src="./img/download.jpg"></a>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
         </div>
 
-        <div class="vare3">
+        <div class="vare">
             <h1>Vare 3</h1>
-            <img src="./img/download.jpg">
+            <a><img src="./img/download.jpg"></a>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
         </div>
-    </div>
+        <div class="vare">
+            <h1>Vare 4</h1>
+            <a><img src="./img/download.jpg"></a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
+        
+        <div class="vare">
+            <h1>Vare 5</h1>
+            <a><img src="./img/download.jpg"></a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
 
+        <div class="vare">
+            <h1>Vare 6</h1>
+            <a><img src="./img/download.jpg"></a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum harum, voluptas, nam sit ea nisi laboriosam similique magni, nihil asperiores quod in molestias? Laboriosam est illum laborum deserunt expedita?</p>
+        </div>
+
+    </div>
 </body>
     
 
--- nettside/pages/index.php
+++ nettside/pages/index.php
@@ -5,6 +5,30 @@
   <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+<h1 class="HovedTekst">Clausens Nettbutikk</h1>
+
     
+<div class="innskudd">
+    <form action="index.php" method="post">
+        Navn: <input type="text" name="navn"><br>
+        E-post <input type="text" name="E-post"><br>
+        <input type="submit">
+    </form>
+</div>
+
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    //lager en connection med databasen
+    $conn = mysqli_connect($servername, $username, $password); 
+
+    //sjekker forbinnelsen med serveren
+    if(!$conn){
+        die("Connection Failed: " . mysqli_connect_error());
+    }
+    echo "Connected Sucesfully";
+?>
 </body>
 </html>
