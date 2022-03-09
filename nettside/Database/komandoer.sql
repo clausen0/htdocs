bb47f7d27ec70c7f9a79178d54ef5bf9a92ed149
clausen0 <sancla@live.no>
2022-03-09 13:40:16 +0100

    lagd inn et par fler sql komandoer

--- nettside/Database/komandoer.sql
+++ nettside/Database/komandoer.sql
@@ -1,24 +1,35 @@
-create database "BuSy";
-use "BuSy";
+create database `BuSy`;
+use `BuSy`;
 
-create table if not exists "Firmaer"(
-    "id" INT not null auto_increment,
-    "Navn" varchar not null,
-    "Adresse" varchar not null,
-    "Orgnummer" INT not null,
-    "Nummer" varchar not null.
-    "web" varchar not null,
-    primary key(id)
+create table if not exists `Firmaer`(
+    `id` INT not null auto_increment,
+    `Navn` varchar not null,
+    `Adresse` varchar not null,
+    `Orgnummer` INT not null,
+    `Nummer` varchar not null.
+    `web` varchar not null,
+    primary key(`id`)
 )
 
-create table if not exists  "Personer"(
-    "id" INT not null auto_increment,
-    "Navn" varchar(50) not null,
-    "Epost" varchar(50) not null,
-    "Nummer" int not null, 
-    "kjønn" varchar(25) not null,
-    "stilling" varchar(50) not null,
-    "Firma_Id" int not null,
-    primary key("id"),
-    foreign key("Firma_Id") references("id")
+create table if not exists  `Personer`(
+    `id` INT not null auto_increment,
+    `Navn` varchar(50) not null,
+    `Epost` varchar(50) not null,
+    `Nummer` int not null, 
+    `kjønn` varchar(25) not null,
+    `stilling` varchar(50) not null,
+    `Firma_Id` int not null,
+    primary key(`id`),
+    foreign key(`Firma_Id`) references `Firmaer`(`id`)
 );
+
+
+create table if not exists `Produkt`(
+    `id` INT not null auto_increment,
+    `produktbetegnelse` varchar not null,
+    `PrisInn` varchar not null,
+    `PrisUt` varchar not null,
+    `Levrandør` varchar not null,
+    primary key (`id`),
+    foreign key(`Levrandør`) references 'Firmaer'(`id`)
+)
