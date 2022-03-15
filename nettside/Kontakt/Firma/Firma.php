b5ff4aa7f5486fca53e217adf3f3ef0c4e6a388c
clausen0 <sancla@live.no>
2022-03-15 10:04:54 +0100

    Bare hyggelig Karlsen babe ;)

---
+++ nettside/Kontakt/Firma/Firma.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style.css?<?=filemtime("../../style.css")?>">
+</head>
+<body>
+    <h1 class="HovedTekst">Clausens Nettbutikk</h1>
+
+            
+    <div class="innskudd">
+        <div class="forum">
+            <form action="index.php" method="post">
+                <label for="Navn">Navn:</label><br>
+                <input type="text" id="Navn" name="navn"><br>
+                <label for="Adresse">Adresse:</label><br>
+                <input type="text" id="Adresse" name="Adresse"><br>
+                <label for="OrgNummer">OrgNummer:</label><br>
+                <input type="text" id="OrgNummer" name="OrgNummer"><br>
+                <label for="nummer">Nummer:</label><br>
+                <input type="text" id="nummer" name="Nummer"><br>
+                <label for="web">Nettside:</label><br>
+                <input type="text" id="web" name="web"><br>
+                <input type="submit">
+                <input type="reset">
+            </form>
+        </div>
+    </div>
+
+    <?php 
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        //lager en connection med databasen
+        $conn = mysqli_connect($servername, $username, $password); 
+
+        //sjekker forbinnelsen med serveren
+        if(!$conn){
+            die("Connection Failed: " . mysqli_connect_error());
+        }
+        echo "Connected Sucesfully";
+    ?>
+
+</body>
+</html>
---
+++ nettside/Kontakt/Person/person.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style.css?<?=filemtime("../../style.css")?>">
+</head>
+<body>
+    <h1 class="HovedTekst">Clausens Nettbutikk</h1>
+
+            
+    <div class="innskudd">
+        <div class="forum">
+            <form action="index.php" method="post">
+                <label for="Navn">Navn:</label><br>
+                <input type="text" id="Navn" name="navn"><br>
+                <label for="Epost">E-post adresse:</label><br>
+                <input type="text" id="Epost" name="E-post"><br>
+                <label for="nummer">Nummer:</label><br>
+                <input type="text" id="nummer" name="Nummer"><br>
+                <label for="gender">Kjønn:</label>
+                <div class="kjonn">
+                    <input type="radio" id="gender" name="gender">Mann 
+                    <input type="radio" id="gender" name="gender">Kvinne 
+                    <input type="radio" id="gender" name="gender">Annet 
+                </div><br>
+                <h2>Jobber Du i et firma?</h2>
+                <label for="Firma">Firma:</label><br>
+                <input type="text" id="Firma" name="Firma"><br>
+                <label for="Stilling">Stilling:</label><br>
+                <input type="text" id="Stilling" name="Stilling"><br>
+                <input type="submit">
+                <input type="reset">
+            </form>
+        </div>
+    </div>
+
+    <?php 
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        //lager en connection med databasen
+        $conn = mysqli_connect($servername, $username, $password); 
+
+        //sjekker forbinnelsen med serveren
+        if(!$conn){
+            die("Connection Failed: " . mysqli_connect_error());
+        }
+        echo "Connected Sucesfully";
+    ?>
+
+</body>
+</html>
