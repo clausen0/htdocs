2107498a14cceac2ad3dbf0f549ebc502268a9ec
clausen0 <sancla@live.no>
2022-04-05 18:15:30 +0200

    fjernet en av metodene som ikke fungerte for sql

--- nettside/Kontakt/Person/person.php
+++ nettside/Kontakt/Person/person.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="../../style.css?<?=filemtime("../../style.css")?>">
+    <link rel="stylesheet" href="../../style.css?<?= filemtime("../../style.css") ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
     <ul class="topnav">
         <li><a class="active" href="../../forside.php">Forside</a></li>
         <li><a href="#news">Kontakt modul
-            
-        </a></li>
+
+            </a></li>
         <li><a href="#contact">Contact</a></li>
         <li class="right"><a href="../Kontakt.php">Tilbake</a></li>
     </ul>
     <h1 class="HovedTekst">Clausens Nettbutikk</h1>
 
-            
+
     <div class="innskudd">
         <div class="forum">
             <form action="person.php" method="post">
@@ -28,9 +30,9 @@
                 <input type="text" id="nummer" name="Nummer"><br>
                 <label for="gender">Kjønn:</label>
                 <div class="kjonn">
-                    <input type="radio" id="gender" name="gender">Mann 
-                    <input type="radio" id="gender" name="gender">Kvinne 
-                    <input type="radio" id="gender" name="gender">Annet 
+                    <input type="radio" id="gender" name="gender">Mann
+                    <input type="radio" id="gender" name="gender">Kvinne
+                    <input type="radio" id="gender" name="gender">Annet
                 </div><br>
                 <h2>Jobber Du i et firma?</h2>
                 <label for="Firma">Firma:</label><br>
@@ -43,54 +45,37 @@
         </div>
     </div>
 
-    <?php 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
-        if(isset($_POST['navn']) && isset($_POST['Epost']) && isset($_POST['Nummer']) && isset($_POST['gender']) && isset($_POST['Firma']) && isset($_POST['Stilling'])){
-            $name = $_POST['navn'];
-            $Epost = $_POST['Epost'];
-            $Number = $_POST['Nummer'];
-            $Gender = $_POST['gender'];
-            $Firma = $_POST['Firma'];
-            $Stilling = $_POST['Stilling'];    
-            
-            // //lager en connection med databasen
-            // $conn = mysqli_connect($servername, $username, $password, 'BuSy'); 
-            
-            // //sjekker forbinnelsen med serveren
-            // if(!$conn){
-            //     die("Connection Failed: " . mysqli_connect_error());
-            // }
-            // echo "Connected Sucesfully";
-            
-            // $sql = 'INSERT INTO personer (Navn, Epost, Nummer, kjønn)
-            // VALUES($name, $Epost, $Number, $Gender)';
-    
-            // $conn->query($sql);
-            
-            // Create connection
-            $conn = mysqli_connect($servername, $username, $password, 'BuSy');
-            // Check connection
-            if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-            }
+    if (isset($_POST['navn']) && isset($_POST['Epost']) && isset($_POST['Nummer']) && isset($_POST['gender']) && isset($_POST['Firma']) && isset($_POST['Stilling'])) {
+        $name = $_POST['navn'];
+        $Epost = $_POST['Epost'];
+        $Number = $_POST['Nummer'];
+        $Gender = $_POST['gender'];
+        $Firma = $_POST['Firma'];
+        $Stilling = $_POST['Stilling'];
 
-            $sql = "INSERT INTO MyGuests (Navn, Epost, Nummer, kjønn)
-            VALUES ($name, $Epost, $Number, $Gender)";
+        //lager en connection med databasen
+        $conn = mysqli_connect($servername, $username, $password, 'BuSy');
 
-            if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-            } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-            }
-
-            mysqli_close($conn);
+        //sjekker forbinnelsen med serveren
+        if (!$conn) {
+            die("Connection Failed: " . mysqli_connect_error());
         }
-        
+        echo "Connected Sucesfully";
+
+        $sql = 'INSERT INTO personer (Navn, Epost, Nummer, kjønn)
+            VALUES($name, $Epost, $Number, $Gender)';
+
+        $conn->query($sql);
+    }
+
 
     ?>
 
 </body>
+
 </html>
