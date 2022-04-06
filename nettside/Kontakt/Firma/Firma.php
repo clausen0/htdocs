02076d4f6952fdb0d9d02e9299e06269d187b98f
clausen0 <sancla@live.no>
2022-04-06 16:07:31 +0200

    lagt inn kode for firmaer, funker ikke midlertidig, ingen feilmeldinger

--- nettside/Kontakt/Firma/Firma.php
+++ nettside/Kontakt/Firma/Firma.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="../../style.css?<?=filemtime("../../style.css")?>">
+    <link rel="stylesheet" href="../../style.css?<?= filemtime("../../style.css") ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
     <ul class="topnav">
         <li><a class="active" href="../../forside.php">Forside</a></li>
@@ -14,10 +16,10 @@
     </ul>
     <h1 class="HovedTekst">Clausens Nettbutikk</h1>
 
-            
+
     <div class="innskudd">
         <div class="forum">
-            <form action="index.php" method="post">
+            <form action="Firma.php" method="post">
                 <label for="Navn">Navn:</label><br>
                 <input type="text" id="Navn" name="navn"><br>
                 <label for="Adresse">Adresse:</label><br>
@@ -25,31 +27,50 @@
                 <label for="OrgNummer">OrgNummer:</label><br>
                 <input type="text" id="OrgNummer" name="OrgNummer"><br>
                 <label for="nummer">Nummer:</label><br>
-                <input type="text" id="nummer" name="Nummer"><br>
+                <input type="text" id="Nummer" name="Nummer"><br>
                 <label for="web">Nettside:</label><br>
-                <input type="text" id="web" name="web"><br>
-                <input type="radio" id="type" name="KundeType">Levrandør
-                <input type="radio" id="type" name="KundeType">Kunde<br>
+                <input type="text" id="Web" name="Web"><br>
+                <input type="radio" id="KundeType" name="KundeType">Levrandør<br>
                 <input type="submit">
                 <input type="reset">
             </form>
         </div>
     </div>
 
-    <?php 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
-        //lager en connection med databasen
-        $conn = mysqli_connect($servername, $username, $password); 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['Navn']) && isset($_POST['Adresse']) && isset($_POST['OrgNummer']) && isset($_POST['Nummer']) && isset($_POST['Web']) && isset($_POST['KundeType'])) {
+            $name = $_POST['Navn'];
+            $Adress = $_POST['Adresse'];
+            $OrgNumber = $_POST['OrgNummer'];
+            $Number = $_POST['Nummer'];
+            $Web = $_POST['Web'];
+            $Customer = $_POST['KundeType'];
 
-        //sjekker forbinnelsen med serveren
-        if(!$conn){
-            die("Connection Failed: " . mysqli_connect_error());
+            //lager en connection med databasen
+            $conn = mysqli_connect($servername, $username, $password);
+
+            //sjekker forbinnelsen med serveren
+            if (!$conn) {
+                die("Connection Failed: " . mysqli_connect_error());
+            }
+            echo "Connected Sucesfully";
+
+            $sql = "INSERT INTO firmaer(Navn, Adresse, Orgnummer, Nummer, web, KundeType)
+            VALUES('$name', '$Adress', '$OrgNumber', '$Number', '$Web', '$Customer')";
+
+            echo $sql;
+
+            $conn->query($sql);
         }
-        echo "Connected Sucesfully";
+    }
+
     ?>
 
 </body>
+
 </html>
