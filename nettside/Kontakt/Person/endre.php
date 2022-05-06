6d083e5899d01b3c7941c1ea1ceaca67cc950f8a
clausen0 <sancla@live.no>
2022-05-06 10:11:10 +0200

    ferdig med endring

--- nettside/Kontakt/Person/endre.php
+++ nettside/Kontakt/Person/endre.php
@@ -19,10 +19,9 @@
     <h2>Endre personopplysninger?</h2>
     <div class="innskudd">
         <div class="forum">
-            <form action="person.php" method="post">
-                <label for="Navn">Navn:</label><br>
-                <select>
-                <option selected value="0">Hvem vil du endre på?</option>
+            <form action="endre.php" method="post">
+                <select name="Person">
+                    <option selected value="0">Hvem vil du endre på?</option>
                     <?php
                         $SQL = new mysqli("localhost", "root", "", "busy");
                         if ($SQL->connect_error) die("Connection Failed: " . $conn->connect_error);
@@ -32,21 +31,35 @@
                                 echo "<option value='" . $row["id"] ."'>" . $row["Navn"] . "</option>";
                             }
                         }
-                    ?>
-                </select><br>
+                        ?>
+                </select><br><br>
+                <label for="Navn">Navn:</label><br>
+                <input type="text" id="Navn" name="Navn"><br>
                 <label for="Epost">E-post adresse:</label><br>
                 <input type="text" id="Epost" name="Epost"><br>
                 <label for="nummer">Nummer:</label><br>
                 <input type="text" id="nummer" name="Nummer"><br>
                 <label for="gender">Kjønn:</label>
                 <div class="kjonn">
-                    <input type="radio" id="gender" name="gender">Mann
-                    <input type="radio" id="gender" name="gender">Kvinne
-                    <input type="radio" id="gender" name="gender">Annet
+                    <input type="radio" id="gender" name="gender" value="Mann">Mann
+                    <input type="radio" id="gender" name="gender" value="Kvinne">Kvinne
+                    <input type="radio" id="gender" name="gender" value="Annet">Annet
                 </div><br>
                 <h2>Endre hvilket firma?</h2>
                 <label for="Firma">Firma:</label><br>
-                <input type="text" id="Firma" name="Firma"><br>
+                <select name="Firma" id="Firma">
+                    <option selected value="0">Ingen firma</option>
+                    <?php
+                        $SQL = new mysqli("localhost", "root", "", "busy");
+                        if ($SQL->connect_error) die("Connection Failed: " . $conn->connect_error);
+                        $res = $SQL->query("SELECT `id`, `Navn` FROM firmaer ORDER BY `Navn`;");
+                        if($res->num_rows > 0) {
+                            while($row = $res->fetch_assoc()) {
+                                echo "<option value='" . $row["id"] ."'>" . $row["Navn"] . "</option>";
+                            }
+                        }
+                    ?>
+                </select><br>
                 <label for="Stilling">Stilling:</label><br>
                 <input type="text" id="Stilling" name="Stilling"><br>
                 <input type="submit">
@@ -56,24 +69,18 @@
     </div>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") { //sjekker om metoden for requesten er post og ikke get.
-        if (isset($_POST['navn']) && isset($_POST['Epost']) && isset($_POST['Nummer']) && isset($_POST['gender']) && isset($_POST['Firma']) && isset($_POST['Stilling'])) {
-            $name = $_POST['navn'];
+        print_r($_POST);
+        if (isset($_POST['Person']) && isset($_POST['Navn']) && isset($_POST['Epost']) && isset($_POST['Nummer']) && isset($_POST['gender']) && isset($_POST['Firma']) && isset($_POST['Stilling'])) {
+            $name = $_POST['Navn'];
             $Epost = $_POST['Epost'];
             $Number = $_POST['Nummer'];
             $Gender = $_POST['gender'];
-            $Firma = $_POST['Firma'];
-            $Stilling = $_POST['Stilling'];
+            $Firma = $_POST['Firma'] != 0 ? "" . $_POST['Firma'] : "NULL";
+            $Stilling = strlen($_POST['Stilling']) > 0 ? "'" . $_POST['Stilling'] . "'" : "NULL";
+            $Person = $_POST['Person'];
 
-            //lager en connection med databasen
-            $conn = new mysqli($servername, $username, $password, 'BuSy');
-
-            //sjekker forbinnelsen med serveren
-            if ($conn->connect_error) {
-                die("Connection Failed: " . $conn->connect_error);
-            }
-            echo "Connected Sucesfully";
-
-            $sql = "UPDATE personer ";
+            $sql = "UPDATE `personer` SET `Navn` = '$name', `Epost` = '$Epost', `Nummer` = $Number, `kjønn` = '$Gender', `stilling` = $Stilling, `Firma_Id` = $Firma WHERE `id` = $Person";
+            $SQL->query($sql);
         }
     }
     ?>
